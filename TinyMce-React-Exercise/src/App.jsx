ff41e9d942d022dc0ad0cb7ce68be7f4930f50c2
Rosen Todorov <rosen.vt55@gmail.com>
2023-10-30 13:46:12 +0200

    Implement basic tinyMCE text editor component for main project

---
+++ TinyMce-React-Exercise/src/App.jsx
@@ -0,0 +1,40 @@
+import { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import "./App.css";
+
+function App() {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+  return (
+    <>
+      <Editor
+        apiKey='96xztb739pac7nr7rdpsopcswma57vu98h3dk649gdul0ws8'
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table paste code help wordcount",
+          ],
+          toolbar:
+            "undo redo | formatselect | " +
+            "bold italic backcolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+        }}
+      />
+      <button onClick={log}>Log editor content</button>
+    </>
+  );
+}
+
+export default App;
